4254e85fcc6e0e58e5cccebefdece5d14a3dd1d3
Hoshin <hoshin-casts@proton.me>
2025-08-31 00:07:29 +0200

    chore: update default config

--- front/client-app/src/config.js
+++ front/client-app/src/config.js
@@ -22,6 +22,6 @@ export const DEFAULT_STATE = {
     }
 }
 
-export const DEFAULT_LOGO = 'https://assets.olympe.xyz/assets/organizations/2/profile'
+export const DEFAULT_LOGO = 'https://media.discordapp.net/attachments/1231353263161344090/1400231498912890920/FCE_LG_S6_Logo_V_Black.png?ex=68b36fb4&is=68b21e34&hm=a569b00027bdfa35ce298f219832d379c7f6ca6619767414e9450fba8a8c6686&=&format=webp&quality=lossless&width=754&height=856'
 
 export const WEBSOCKET_URL = 'ws://localhost:3000'
