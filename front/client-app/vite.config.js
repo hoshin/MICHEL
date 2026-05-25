32202bb2c42fa1581ca85d528e10f010fd7d64f7
Hoshin <hoshin-casts@proton.me>
2026-05-25 15:09:15 +0200

    feat(front): add a public route for a custom directory
    Purpose : make custom pictures available. Probably okay locally, might need a little more assesment for a remote hosted instance

--- front/client-app/vite.config.js
+++ front/client-app/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './',
+  base: "./",
+  publicDir: "./src/assets",
   build: {
-    outDir: '../../dist/front'
+    outDir: "../../dist/front",
   },
   plugins: [react()],
-})
+});
